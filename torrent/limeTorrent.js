a91c94ecf76301f7b0b625225684ad0d05b4d31a
valentino_scevaroli <scevaz95@gmail.com>
2023-05-12 20:56:54 +0200

    Update lime

--- torrent/limeTorrent.js
+++ torrent/limeTorrent.js
@@ -4,8 +4,7 @@ const logger = require('pino')()
 
 async function limeTorrent(query, page = '1') {
     var ALLTORRENT = [];
-    const baseUrl = 'https://www.limetorrents.pro'
-    const url = `${baseUrl}/search/all/${query}/seeds/${page}/`;
+    const url = `https://www.limetorrents.pro/search/all/${query}/seeds/${page}/`;
     let html;
     try {
         html = await axios.get(url, headers = {
@@ -18,7 +17,28 @@ async function limeTorrent(query, page = '1') {
 
     const $ = cheerio.load(html.data);
 
-    $('.table2 tbody tr').each(async (i, element) => {
+    $('.table2 tbody tr').each((i, element) => {
+        if (i > 0) {
+            let category_and_age = $(element).find('td').eq(1).text().trim();
+            category_and_age = category_and_age.split('-');
+            let age = category_and_age[0].trim();
+            let category = category_and_age[1].replace('in', '').trim();
+            let torrent = {
+                "Name": $(element).find('div.tt-name').text().trim(),
+                "Size": $(element).find('td').eq(2).text().trim(),
+                "Category": category,
+                "Age": age,
+                "Seeders": $(element).find('td').eq(3).text().trim(),
+                "Leechers": $(element).find('td').eq(4).text().trim(),
+                "Torrent": $(element).find('div.tt-name a').attr('href'),
+                "Url": "https://www.limetorrents.pro" + $(element).find('div.tt-name a').next().attr('href'),
+                "Provider":"limetorrent"
+            }
+            ALLTORRENT.push(torrent);
+        }
+    })
+
+    await $('.table2 tbody tr').each(async (i, element) => {
         if (i > 0) {
             let category_and_age = $(element).find('td').eq(1).text().trim();
             category_and_age = category_and_age.split('-');
@@ -42,7 +62,7 @@ async function limeTorrent(query, page = '1') {
             try{
                 html = await axios.get(link);
             }catch{
-                logget.info(`Limetorrent: Error getting link for ${query}`)
+                logger.info(`Limetorrent: Error getting link for ${query}`)
             }
 
                 const $ = cheerio.load(html.data);
